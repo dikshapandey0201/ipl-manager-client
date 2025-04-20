f4097492d3545393a542935ed24cdff98e0c88c7
dikshapandey0201 <dikshapandey0201@gmail.com>
2025-04-20 17:33:35 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
     proxy: {
       '/api': {
         target: 'https://ipl-manager-server.onrender.com',
-        secure: false,
+        changeOrigin: true,
       }
     }
   }
